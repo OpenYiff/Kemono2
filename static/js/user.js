56ad8af8a3d7ae37011c0256d358cd2f94779501
BassOfBass <vinniethepooloo@protonmail.com>
2021-04-15 05:53:43 +0500

    refactor(template, js): post and artist favorite buttons
    artist and post favorite actions - no page reload
    template: changed fav to buttons
    javascript:
    - `kemonoAPI`
    a dictionary for API methods
    - `kemonoFetchAsync()`
    kemono wrapper for fetch
    right now only handles redirects
    as they are handled by server automatically
    - `initSectionPostDetails()`
    adds listener to fav button
    callback changes the state of it
    - `initSectionUserDetails()`
    same logic as above

--- static/js/user.js
+++ static/js/user.js
@@ -1,29 +1,3 @@
 /* eslint-disable no-unused-vars */
-function favorite_artist(service, user) {
-  fetch(`/favorites/artist/${service}/${user}`, {
-    method: 'POST'
-  }).then(res => {
-    if (res.redirected) {
-      window.location =  add_url_param(res.url, 'redir', window.location.pathname);;
-    } else if (res.ok) {
-      location.reload();
-    } else {
-      alert('Error 003 - could not save favorite');
-    }
-  });
-}
 
-function unfavorite_artist(service, user) {
-  fetch(`/favorites/artist/${service}/${user}`, {
-    method: "DELETE"
-  }).then(res => {
-    if (res.redirected) {
-      window.location =  add_url_param(res.url, 'redir', window.location.pathname);;
-    } else if (res.ok) {
-      location.reload();
-    } else {
-      alert('Error 004 - could not remove favorite');
-    }
-  });
-}
 /* eslint-enable no-unused-vars */
