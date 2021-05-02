d8b4f4665fb44758f28117826e6652ce3922c50f
BassOfBass <vinniethepooloo@protonmail.com>
2021-05-02 20:36:43 +0500

    style(template): further shell work

--- static/js/global.js
+++ static/js/global.js
@@ -1,6 +1,10 @@
-; (() => {
+;(() => {
   "use strict";
 
+  /**
+   * @type {HTMLElement | null}
+   */
+  const globalHeader = document.querySelector(".global-header");
   /**
    * @type {HTMLElement | null}
    */
@@ -10,20 +14,36 @@
   const sectionRegister = document.querySelector(".site-section--register");
 
   // run scripts for present parts
+  globalHeader && initGlobalHeader(globalHeader);
   globalFooter && initGlobalFooter(globalFooter);
   sectionRegister && initSectionRegister();
+
+  /** 
+   * @param {HTMLElement} header
+   */
+  function initGlobalHeader(header) {
+    /**
+     * @type {HTMLUListElement}
+     */
+    const navList = header.querySelector(".nav-list--mid");
+    
+    initNavBar(navList);
+  }
 
   /**
    * @param {HTMLElement} footer 
    */
   function initGlobalFooter(footer) {
     const navBar = footer.querySelector(".global-footer__navigation");
+    const navList = navBar.querySelector(".nav-list--vertical")
     /**
      * @type {HTMLButtonElement | null}
      */
     const navButton = footer.querySelector(".global-footer__nav-button");
 
     navButton.addEventListener("click", switchNavBar)
+
+    initNavBar(navList);
 
     function switchNavBar() {
       navBar.classList.toggle("global-footer__navigation--hidden");
@@ -33,6 +53,57 @@
 
   function initSectionRegister() {
     populate_favorites();
+  }
+
+  /**
+   * @param {HTMLUListElement} navList 
+   */
+  function initNavBar(navList) {
+    /**
+     * @type {HTMLUListElement | null}
+     */
+    let currentSelection;
+
+    navList.addEventListener("click", switchSubmenu);
+    navList.addEventListener("keydown", closeAllSublists)
+    /**
+     * @param {MouseEvent} event 
+     */
+    function switchSubmenu(event) {
+      event.stopPropagation();
+      /**
+       * @type {HTMLButtonElement}
+       */
+      const button = event.target;
+
+      if (button.classList.contains("nav-list__button")) {
+        const navItem = button.closest(".nav-list__item");
+        const subList = navItem.querySelector(".nav-list__sublist");
+
+        if (currentSelection && currentSelection !== subList) {
+          currentSelection.classList.add("nav-list__sublist--hidden");
+        }
+
+        if (subList.classList.contains("nav-list__sublist--hidden")) {
+          subList.classList.remove("nav-list__sublist--hidden");
+          currentSelection = subList;
+        } else {
+          subList.classList.add("nav-list__sublist--hidden");
+          currentSelection = null;
+        }
+        
+      }
+    }
+
+    /**
+     * @param {KeyboardEvent} event 
+     */
+    function closeAllSublists(event) {
+      if (currentSelection && (event.key === "Esc" || event.key === "Escape")) {
+        currentSelection.classList.add("nav-list__sublist--hidden")
+      }
+      
+    }
   }
 
   function populate_favorites() {
